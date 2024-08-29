db81adc2bfd879e27d0002581defff7b26dd4893
Leonard Chi <leonardocompsci@gmail.com>
2024-08-29 14:58:46 +0800

    Create GameController.java

---
+++ Java/Minesweeper/src/main/java/minesweeper/GameController.java
@@ -0,0 +1,20 @@
+package minesweeper;
+
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
+
+public class GameController {
+    private GameBoard gameBoard;
+
+    public GameController(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public void handleMousePress(MouseEvent e) {
+        // Handle mouse press events
+    }
+
+    public void handleKeyPress(KeyEvent e) {
+        // Handle key press events
+    }
+}
